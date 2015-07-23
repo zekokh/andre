9eb926e0bf0bc1d8c30a02b694c50f98805de950
zekokh <heroin160892>
2015-07-23 18:09:26 +0300

    add validation in Partner and Provider

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,2 +1,3 @@
 class Partner < ActiveRecord::Base
+  validates :name, :contact_name, :phone, :address, presence: true
 end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,2 +1,3 @@
 class Provider < ActiveRecord::Base
+  validates :name, :contact_name, :phone, :address, presence: true
 end
